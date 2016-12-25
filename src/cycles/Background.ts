6cba68d19d162c51fc655c2148ba0878ec5d349f
Brenton Simpson <appsforartists@google.com>
2016-12-25 14:24:21 -0800

    [added] first draft of note$

--- src/cycles/Background.ts
+++ src/cycles/Background.ts
@@ -31,6 +31,17 @@ export default function Background({ messages: message$, pianoConnection: pianoE
     message => message.type === MessageType.PLAY_SONG
   ).pluck('payload');
 
+  // `song$` dispatches values in the shape:
+  //
+  //   {
+  //     [time]: {
+  //       [trackID]: note,
+  //     },
+  //   }
+  //
+  // so we can queue notes in decisecond increments and change which tracks are
+  // included as the song is playing.
+
   const song$ = requestedSongURL$.flatMap(
     url => Observable.fromPromise(
       fetch(url).then(
@@ -45,13 +56,71 @@ export default function Background({ messages: message$, pianoConnection: pianoE
         return Observable.empty();
       }
     )
+  ).map(
+    song => {
+      const notesByTrackByTime = {};
+      let duration = 0;
+
+      song.tracks.forEach(
+        (track, trackID) => {
+          duration = Math.max(track.duration, duration);
+
+          track.notes.forEach(
+            note => {
+              const time = note.time * 1000;
+              const roundedTime = Math.floor(note.time * 10) * 100;
+
+              if (!notesByTrackByTime[roundedTime]) {
+                notesByTrackByTime[roundedTime] = {};
+              }
+
+              if (!notesByTrackByTime[roundedTime][trackID]) {
+                notesByTrackByTime[roundedTime][trackID] = [];
+              }
+
+              notesByTrackByTime[roundedTime][trackID].push(
+                {
+                  note: note.midi,
+                  duration: note.duration * 1000,
+                  velocity: note.velocity,
+                  time,
+                }
+              );
+            }
+          )
+        }
+      );
+
+      notesByTrackByTime.length = Math.round(duration * 1000);
+      return notesByTrackByTime;
+    }
+  ).publishReplay();
+
+  // A MemoryStream in xstream is a publishReplay() + connect() in RxJS
+  song$.connect();
+
+  const playbackStartingTime$ = song$.map(
+    () => performance.now()
   );
 
-  song$.subscribe(console.log);
+  // TODO: clean this up.
+  const note$ = playbackStartingTime$.flatMap(
+    startingTime => Observable.interval(100).map(
+      count => count * 100
+    ).withLatestFrom(song$).takeWhile(
+      ([ time, song ]) => song.length > time
+    ).map(
+      ([ time, song ]) => song[time]
+    ).filter(value => value !== undefined)
+  ).flatMap(
+    notesByTrack => Observable.of(
+      ...[].concat(...Object.values(notesByTrack))
+    )
+  ).do(console.log);
 
   return {
     messages: pianoError$,
-    piano: Observable.empty(),
+    piano: note$,
     pianoConnection: requestedSongURL$,
   }
 }
