f443e97effbc852a5669d0cc46f3b9efa7412789
Brenton Simpson <appsforartists@google.com>
2016-12-27 13:13:06 -0800

    [added] experiment using raw midi-file-parser instead of midiconvert

--- src/cycles/Background.ts
+++ src/cycles/Background.ts
@@ -18,52 +18,8 @@ import {
   Observable,
 } from 'rxjs';
 
-import * as MIDIConvert from 'midiconvert';
-
-declare namespace MIDIConvert {
-  export type Note = {
-    time: number,
-    name: string,
-    midi: number,
-    velocity: number,
-    duration: number,
-  };
-
-  export type Track = {
-    name: string,
-    instrument: string,
-    notes: Array<Note>,
-    duration: number,
-    length: number,
-  };
-
-  export type ControlChange = {
-    time: number,
-    name: string,
-    midi: number,
-    velocity: number,
-    duration: number,
-  };
-
-  export type MIDI = {
-    header: {
-      bpm: number,
-      timeSignature: [number, number],
-      PPQ: number,
-    },
-    duration: number,
-
-    tracks: Array<Track>,
-
-    controlChanges: {
-      [key: number]: ControlChange
-    },
-  };
-
-  export function parse(raw: ArrayBuffer): MIDI;
-  export function load(url: string, data?: any, method?: 'GET'|'POST'): Promise<MIDI>;
-  export function create(): MIDI;
-}
+// import * as MIDIConvert from 'midiconvert';
+import * as parseMIDI from 'midi-file-parser';
 
 import {
   Dict,
@@ -111,7 +67,9 @@ export default function Background({ messages: message$, pianoConnection: pianoA
       fetch(url).then(
         (response: Response) => response.arrayBuffer()
       ).then(
-        MIDIConvert.parse
+        (arrayBuffer) => parseMIDI(
+          String.fromCharCode(...new Uint8Array(arrayBuffer))
+        )
       )
     // Prevent Promise errors from breaking the stream
     ).catch(
@@ -119,49 +77,51 @@ export default function Background({ messages: message$, pianoConnection: pianoA
         console.error(error);
         return Observable.empty();
       }
-    ).takeUntil(
-      pianoIsOffline$
+    // ).takeUntil(
+    //   pianoIsOffline$
+    ).map(
+      midi => Observable.from(midi.tracks).flatMap(track => Observable.from(track))
     )
-  ).do(console.log).map(
-    (song: MIDIConvert.MIDI) => {
-      const notesByTrackByTime:Dict<Dict<any>> = {};
-      let duration = 0;
+  // ).do(console.log).map(
+  //   (song: MIDIConvert.MIDI) => {
+  //     const notesByTrackByTime:Dict<Dict<any>> = {};
+  //     let duration = 0;
 
-      song.tracks.forEach(
-        (track, trackID) => track.notes.forEach(
-          note => {
-            const time = note.time * 1000;
-            const roundedTime = Math.floor(note.time * 10) * 100;
+  //     song.tracks.forEach(
+  //       (track, trackID) => track.notes.forEach(
+  //         note => {
+  //           const time = note.time * 1000;
+  //           const roundedTime = Math.floor(note.time * 10) * 100;
 
-            if (!notesByTrackByTime[roundedTime]) {
-              notesByTrackByTime[roundedTime] = {};
-            }
+  //           if (!notesByTrackByTime[roundedTime]) {
+  //             notesByTrackByTime[roundedTime] = {};
+  //           }
 
-            if (!notesByTrackByTime[roundedTime][trackID]) {
-              notesByTrackByTime[roundedTime][trackID] = [];
-            }
+  //           if (!notesByTrackByTime[roundedTime][trackID]) {
+  //             notesByTrackByTime[roundedTime][trackID] = [];
+  //           }
 
-            notesByTrackByTime[roundedTime][trackID].push(
-              {
-                note: note.midi,
-                duration: note.duration * 1000,
-                velocity: note.velocity,
-                time,
-              }
-            );
-          }
-        )
-      );
+  //           notesByTrackByTime[roundedTime][trackID].push(
+  //             {
+  //               note: note.midi,
+  //               duration: note.duration * 1000,
+  //               velocity: note.velocity,
+  //               time,
+  //             }
+  //           );
+  //         }
+  //       )
+  //     );
 
-      notesByTrackByTime.length = Math.round(song.duration * 1000);
-      return notesByTrackByTime;
-    }
+  //     notesByTrackByTime.length = Math.round(song.duration * 1000);
+  //     return notesByTrackByTime;
+  //   }
   ).publishReplay();
 
-  // Streams that represent properties (as opposed to events) should be
-  // memoized.  In xstream, you'd accomplish this with a MemoryStream.  The
-  // equivalent in RxJS is a publishReplay() + connect().  connect returns a
-  // subscription, so we have to do it on its own line.
+  // // Streams that represent properties (as opposed to events) should be
+  // // memoized.  In xstream, you'd accomplish this with a MemoryStream.  The
+  // // equivalent in RxJS is a publishReplay() + connect().  connect returns a
+  // // subscription, so we have to do it on its own line.
   song$.connect();
 
   const playStartingTime$ = Observable.merge(playRequest$, song$).map(
@@ -171,39 +131,63 @@ export default function Background({ messages: message$, pianoConnection: pianoA
   // takeUntil doesn't seem to be working as you'd expect it to.
   const playCurrentTime$$ = playStartingTime$.map(
     startingTime => Observable.interval(100).map(
-      count => count * 100
-    ).withLatestFrom(song$).takeWhile(
-      ([ time, song ]) => song.length > time
-    ).map(
-      ([ time ]) => time
-    ).takeUntil(
-      Observable.merge(
-        stopRequest$,
-        pianoIsOffline$,
-      )
+      count => count * 100 // + 280000 // to start at good part of HR2
+    // ).withLatestFrom(song$).takeWhile(
+    //   ([ time, song ]) => song.length > time
+    // ).map(
+    //   ([ time ]) => time
+    // ).takeUntil(
+    //   Observable.merge(
+    //     stopRequest$,
+    //     pianoIsOffline$,
+    //   )
     )
-  );
+  ).do(console.log);
 
   const songStopped$ = playCurrentTime$$.flatMap(
     interval$ => interval$.last()
   );
 
-  const note$ = playCurrentTime$$.switch().withLatestFrom(song$).map(
-    ([ time, song ]) => song[time]
-  ).filter(
-    value => value !== undefined
-  ).flatMap(
-    notesByTrack => Observable.of(
-      ...[].concat(...Object.values(notesByTrack))
+  // const note$ = playCurrentTime$$.switch().withLatestFrom(song$).map(
+  //   ([ time, song ]) => song[time]
+  // ).filter(
+  //   value => value !== undefined
+  // ).flatMap(
+  //   notesByTrack => Observable.from(
+  //     [...(notesByTrack[2] || []), ...(notesByTrack[3] || []), ...(notesByTrack[4] || [])]
+  //   )
+  // ).withLatestFrom(playStartingTime$).map(
+  //   ([ note, startTime ]) => (
+  //     {
+  //       ...note,
+  //       time: 100 + startTime + note.time,
+  //     }
+  //   )
+  // ).do(console.log);
+
+  const note$ = song$.flatMap(
+    (event$: Observable<any>) => event$.scan(
+      ({ time }, event) => (
+        {
+          ...event,
+          time: time + event.deltaTime,
+        }
+      ),
+      {
+        time: 0,
+      }
+    ).filter(
+      event => ( event.subtype || '' ).startsWith('note')
     )
   ).withLatestFrom(playStartingTime$).map(
-    ([ note, startTime ]) => (
+    ([ event, startTime ]) => (
       {
-        ...note,
-        time: startTime + note.time,
+        ...event,
+        note: event.noteNumber,
+        // time: startTime + event.time,
       }
     )
-  ).do(console.log);
+  );
 
   const currentPlaybackStatus$ = playStartingTime$.mapTo(PlaybackStatus.PLAYING).merge(
     songStopped$.mapTo(PlaybackStatus.STOPPED)
